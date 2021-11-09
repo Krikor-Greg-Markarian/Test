e5908cdf5a1bd7fcbbfbafe8ec922f64f2b90360
Greg <greg_markarian@hotmail.com>
2021-11-09 17:30:28 +0200

    test

--- components/WinsIcon.js
+++ components/WinsIcon.js
@@ -6,9 +6,8 @@ import HomeIcon from "./HomeIcon";
 const WinsIcon = (props) => {
   return (
     <div className={cn(`w-${props.width} h-${props.height} `, props.color)}>
-      <div className={cn(" text-xs text-center font-bold")}>
-        <div className={cn("")}>{props.icon}</div>
- 
+      <div className={cn("text-xs text-center font-bold")}>
+        {props.icon}
         <div className={cn("pt-2")}>{props.title}</div>
       </div>
     </div>
